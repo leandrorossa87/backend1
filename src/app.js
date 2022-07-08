cd36784fb0af036bf0a02cadd419ee5d62e86a12
leandrorossa87 <leandrorossa87@gmail.com>
2022-07-07 21:06:55 -0300

    incluimos html

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
+const path= require('path')
 
 require('dotenv').config()
-
 const express = require('express');
+
 const app = express();
+
+app.get('/', function(solicitud, respuesta){
+    respuesta.sendFile(path.join(__dirname, './views/index.html'))
+})
 
 app.listen(process.env.PUERTO , function (){
     console.log('Servidor Iniciado en puerto' + process.env.PUERTO)
