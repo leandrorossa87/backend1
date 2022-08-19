4d67e150a1f1cdc08a5ce18a083e3ba349b0449e
leandrorossa87 <leandrorossa87@gmail.com>
2022-08-18 23:53:18 -0300

    rearmado de servidor

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
-const path= require('path')
+const path = require('path')
 
-require('dotenv').config()
-const express = require('express');
-
+const express = require('express')
 const app = express();
 
-app.get('/', function(solicitud, respuesta){
-    respuesta.sendFile(path.join(__dirname, './views/index.html'))
-})
+require('dotenv').config()
 
-app.listen(process.env.PUERTO , function (){
-    console.log('Servidor Iniciado en puerto' + process.env.PUERTO)
+app.get('/', function(solicitud, respuesta) {
+    respuesta.sendFile(path.join(__dirname, 'views/index.html'))
+})  
+
+app.get('/contacto', function(solicitud, respuesta) {
+    respuesta.sendFile(path.join(__dirname, 'views/contacto.html'))
+}) 
+   
+
+
+app.listen(process.env.PUERTO,  function () {
+    console.log('serivdor iniciado en: '+ process.env.PUERTO)
 })
 
